52c708eb42d65a0fe35fd16895c17a8a38c672d4
cdobrescu <costin.dobrescu@cloud.ionos.com>
2020-08-28 13:55:07 +0300

    first commit

---
+++ src/main/java/com/ionos/Application.java
@@ -0,0 +1,10 @@
+package com.ionos;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
